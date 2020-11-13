01168f814d8d63392798c585e74b36193a84f75c
Mesrar <mesrar.mohamed@gmail.com>
2020-11-13 10:23:20 +0100

    fix

--- plants_api_demo/rest_api_demo/app.py
+++ plants_api_demo/rest_api_demo/app.py
@@ -8,6 +8,7 @@ from api.plants.endpoints.plants import ns as plants_namespace
 from api.plants.endpoints.imageapi import ns as imgapi_namespace
 from api.restplus import api
 from flask.helpers import get_env
+from flask_cors import CORS, cross_origin
 from flask_restx import Api
 
 from flask_restx.apidoc import apidoc
@@ -16,44 +17,14 @@ URL_PREFIX = '/api'
 apidoc.url_prefix = URL_PREFIX
 
 app = Flask(__name__, static_folder="../build", static_url_path="/")
-
+CORS(app, allow_headers=['Content-Type', 'Access-Control-Allow-Origin',
+                         'Access-Control-Allow-Headers', 'Access-Control-Allow-Methods'])
 
 app.config['MONGOALCHEMY_DATABASE'] = 'library'
 
 logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '../logging.conf'))
 logging.config.fileConfig(logging_conf_path)
 log = logging.getLogger(__name__)
-
-
-try:
-    from flask_cors import CORS  # The typical way to import flask-cors
-except ImportError:
-    # Path hack allows examples to be run without installation.
-    import os
-    parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    os.sys.path.insert(0, parentdir)
-from flask_cors import CORS
-
-public_routes = Blueprint('public', __name__)
-
-
-@public_routes.route("/api/*")
-def helloWorld():
-    '''
-        Since the path '/' does not match the regular expression r'/api/*',
-        this route does not have CORS headers set.
-    '''
-    return '''<h1>Hello CORS!</h1> Read about my spec at the
-<a href="http://www.w3.org/TR/cors/">W3</a> Or, checkout my documentation
-on <a href="https://github.com/corydolphin/flask-cors">Github</a>'''
-
-
-api_v1 = Blueprint('API_v1', __name__)
-
-logging.basicConfig(level=logging.INFO)
-app = Flask('FlaskCorsBlueprintBasedExample')
-app.register_blueprint(api_v1)
-app.register_blueprint(public_routes)
 
 
 @app.after_request
@@ -65,7 +36,6 @@ def apply_caching(response):
         "Access-Control-Allow-Headers,  Access-Control-Allow-Origin, Origin,Accept, " + \
         "X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
     return response
-
 
 @app.route('/')
 def index():
@@ -80,10 +50,15 @@ def configure_app(flask_app):
     flask_app.config['RESTPLUS_VALIDATE'] = settings.RESTPLUS_VALIDATE
     flask_app.config['RESTPLUS_MASK_SWAGGER'] = settings.RESTPLUS_MASK_SWAGGER
     flask_app.config['ERROR_404_HELP'] = settings.RESTPLUS_ERROR_404_HELP
+    flask_app.config['CORS_HEADERS'] = '*';
 
 
 def initialize_app(flask_app):
     configure_app(flask_app)
+
+    blueprint = Blueprint('api', 'blueprint_name', url_prefix=URL_PREFIX)
+    api = Api(blueprint, doc='/doc/')
+    app.register_blueprint(blueprint)
 
     api.add_namespace(plants_namespace)
     api.add_namespace(imgapi_namespace)
@@ -91,11 +66,9 @@ def initialize_app(flask_app):
 
 def main():
     initialize_app(app)
+
     log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(app.config['SERVER_NAME']))
-    if get_env() == 'production':
-        app.run(host='0.0.0.0', port=5000, debug=False)
-    else:
-        app.run(debug=settings.FLASK_DEBUG)
+    app.run(debug=settings.FLASK_DEBUG)
 
 
 if __name__ == "__main__":
