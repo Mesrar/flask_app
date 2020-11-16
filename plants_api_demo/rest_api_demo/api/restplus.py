84155371bc6895ac16624bc34e13c7b8a1dd869b
Mesrar <mesrar.mohamed@gmail.com>
2020-11-16 17:22:46 +0100

    swagger

--- plants_api_demo/rest_api_demo/api/restplus.py
+++ plants_api_demo/rest_api_demo/api/restplus.py
@@ -1,2 +1,23 @@
+import logging
+
+from flask_restx import Api
+import settings
+
+
+log = logging.getLogger(__name__)
+
+api = Api(version='1.0', title='Plants API DEMO',
+          description='Plants API DEMO with Flask RestPlus powered API')
+
+
+
+
+@api.errorhandler
+def default_error_handler(e):
+    message = 'An unhandled exception occurred.'
+    log.exception(message)
+
+    if not settings.FLASK_DEBUG:
+        return {'message': message}, 500
 
 
