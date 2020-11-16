c4839f62dd84f42393c23b689e54adf8c0abbdd5
Mesrar <mesrar.mohamed@gmail.com>
2020-11-16 17:20:54 +0100

    swagger

--- plants_api_demo/rest_api_demo/api/restplus.py
+++ plants_api_demo/rest_api_demo/api/restplus.py
@@ -1,20 +1,2 @@
-import logging
-
-import settings
-
-
-log = logging.getLogger(__name__)
-
-
-
-
-
-@api.errorhandler
-def default_error_handler(e):
-    message = 'An unhandled exception occurred.'
-    log.exception(message)
-
-    if not settings.FLASK_DEBUG:
-        return {'message': message}, 500
 
 
